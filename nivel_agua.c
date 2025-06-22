8c8de00dbf7ce77307c9f9833497e0f7ee232b6f
Hugo <hugo_martins@cepedi.org.br>
2025-06-21 21:34:18 -0300

    Adicionado a funcionalidade web

--- nivel_agua.c
+++ nivel_agua.c
@@ -1,33 +1,156 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/time.h"
+#include <string.h>
+#include "pico/cyw43_arch.h"
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+#include "lwip/netif.h"  
+#include "hardware/adc.h"
+#include "ssd1306.h" 
+#include "hardware/i2c.h"      
 
 #define botao_a 5
 #define botao_b 6
 #define green_led 11
 #define interrupcao(bot) gpio_set_irq_enabled_with_callback(bot, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &gpio_irq_handler)
 
+#define WIFI_SSID "S.F.C 2"
+#define WIFI_PASSWORD "857aj431"
+
+#define I2C_PORT_DISP i2c1
+#define I2C_SDA_DISP 14
+#define I2C_SCL_DISP 15
+#define endereco 0x3C
+
 typedef struct {
     uint8_t min;
     uint8_t max;
     volatile bool estado_bomba;
+    uint8_t nivel_atual;
     absolute_time_t alarm_a;
 } nivel_agua;
 
-nivel_agua nv = {20, 80, false};
+nivel_agua nv = {20, 80, false, 30, 0};
 
+void init_led(void); // Inicialização do LED
 void init_bot(void); // Inicialização dos botões
 void gpio_irq_handler(uint gpio, uint32_t events);  // Tratamento de interrupção
+static err_t tcp_server_accept(void *arg, struct tcp_pcb *newpcb, err_t err); // Função de callback ao aceitar conexões TCP
+static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err); // Função de callback para processar requisições HTTP
+void user_request(char **request); // Tratamento do request do usuário
 int64_t botao_pressionado(alarm_id_t, void *user_data); // Ativo da função após os 2 segundos
+uint8_t xcenter_pos(char *text); // Função para centralizar o texto no display
 
 int main(){
     stdio_init_all();
+    init_led();
     init_bot();
     interrupcao(botao_a);
     interrupcao(botao_b);
+
+    i2c_init(I2C_PORT_DISP, 400 * 1000);
+    gpio_set_function(I2C_SDA_DISP, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_DISP, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_DISP);
+    gpio_pull_up(I2C_SCL_DISP);
+
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT_DISP);
+    ssd1306_config(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_draw_string(&ssd, "Iniciando Wi-Fi", 0, 0);
+    char aguarde[] = "Aguarde...";
+    ssd1306_draw_string(&ssd, aguarde, xcenter_pos(aguarde), 30);    
+    ssd1306_send_data(&ssd);
+
+    if (cyw43_arch_init())
+    {
+        ssd1306_fill(&ssd, false);
+        ssd1306_draw_string(&ssd, "WiFi => FALHA", xcenter_pos("WiFi => FALHA"), 0);
+        ssd1306_send_data(&ssd);
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+    while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 20000))
+    {
+        ssd1306_fill(&ssd, false);
+        ssd1306_draw_string(&ssd, "WiFi => ERRO", 0, 0);
+        ssd1306_send_data(&ssd);
+        return 1;
+    }
+
+    uint8_t *ip = (uint8_t *)&(cyw43_state.netif[0].ip_addr.addr);
+    char ip_str[24];
+    snprintf(ip_str, sizeof(ip_str), "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+
+    ssd1306_fill(&ssd, false);
+    ssd1306_draw_string(&ssd, "WiFi => OK", 0, 0);
+    ssd1306_draw_string(&ssd, ip_str, 0, 10);
+    ssd1306_send_data(&ssd);
+
+    char str_x[14];
+    char str_y[14];
+    char str_pb[14];
+    bool cor = true;
+
+    char ip_text[] = "IP:";
+    strcat(ip_text, ip_str);
+
+    // Inicializa o servidor TCP
+    struct tcp_pcb *server = tcp_new();
+    if (!server) {
+        printf("Falha ao criar servidor TCP\n");
+        return 1;
+    }
+    if (tcp_bind(server, IP_ADDR_ANY, 80) != ERR_OK) {
+        printf("Falha ao associar servidor TCP à porta 80\n");
+        return 1;
+    }
+    server = tcp_listen(server);
+    tcp_accept(server, tcp_server_accept);
+    printf("Servidor ouvindo na porta 80\n");
+
     while (true) {
+        sprintf(str_x, "Nivel Min: %d", nv.min);    
+        sprintf(str_y, "Nivel Max: %d", nv.max);    
+        sprintf(str_pb, "Agua: %d", nv.nivel_atual);
+
+        ssd1306_fill(&ssd, !cor);               
+        ssd1306_rect(&ssd, 0, 0, 127, 63, cor, !cor);
+        ssd1306_draw_string(&ssd, str_pb, 2, 2);
+        ssd1306_draw_string(&ssd, str_x, 2, 10);
+        ssd1306_draw_string(&ssd, str_y, 2, 18);
+
+        if (nv.nivel_atual >= nv.max) {
+            ssd1306_draw_string(&ssd, "Bomba: OFF", 2, 26);
+        } else if (nv.nivel_atual < nv.min) {
+            ssd1306_draw_string(&ssd, "Bomba: ON", 2, 26);
+        } else {
+            ssd1306_draw_string(&ssd, "Bomba: OFF", 2, 26);
+        }
+
+        ssd1306_hline(&ssd, 0, 127, 40, cor);
+
+        ssd1306_draw_string(&ssd, ip_text, xcenter_pos(ip_text), 42);
+
+        ssd1306_draw_string(&ssd, "EmbarcaTech", xcenter_pos("EmbarcaTech"), 50); // Desenha o IP
+
+        ssd1306_send_data(&ssd); 
+
+        cyw43_arch_poll();
         sleep_ms(1000);
     }
+
+    cyw43_arch_deinit();
+    return 0;
+}
+
+void init_led(void){
+    gpio_init(green_led);
+    gpio_set_dir(green_led, GPIO_OUT);
+    gpio_put(green_led, 0);
 }
 
 void init_bot(void){
@@ -67,4 +190,64 @@ int64_t botao_pressionado(alarm_id_t, void *user_data) {
         printf("Botão A pressionado por 2 segundos!\n");
     }
     return 0;
+}
+
+uint8_t xcenter_pos(char* text) {
+    return (WIDTH - 8 * strlen(text)) / 2; // Calcula a posição centralizada
+}
+
+static err_t tcp_server_accept(void *arg, struct tcp_pcb *newpcb, err_t err){
+    tcp_recv(newpcb, tcp_server_recv);
+    return ERR_OK;
+}
+
+void user_request(char **request){
+    if (strstr(*request, "GET /green_on") != NULL) gpio_put(green_led, 1);
+    else if (strstr(*request, "GET /green_off") != NULL) gpio_put(green_led, 0);
+}
+
+static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err){
+
+    if (!p){
+        tcp_close(tpcb);
+        tcp_recv(tpcb, NULL);
+        return ERR_OK;
+    }
+
+    char *request = (char *) malloc(p->len + 1);
+    memcpy(request, p->payload, p->len);
+    request[p->len] = '\0';
+
+    printf("Request: %s\n", request);
+    user_request(&request);
+
+        char html[1024];
+    snprintf(html, sizeof(html),
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/html\r\n"
+             "\r\n"
+             "<!DOCTYPE html>\n"
+             "<html>\n"
+             "<head>\n"
+             "<title> Embarcatech - LED Control </title>\n"
+             "<style>\n"
+             "body { background-color: #b5e5fb; font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }\n"
+             "h1 { font-size: 64px; margin-bottom: 30px; }\n"
+             "button { background-color: LightGray; font-size: 36px; margin: 10px; padding: 20px 40px; border-radius: 10px; }\n"
+             ".temperature { font-size: 48px; margin-top: 30px; color: #333; }\n"
+             "</style>\n"
+             "</head>\n"
+             "<body>\n"
+             "<h1>Embarcatech: LED Control</h1>\n"
+             "<form action=\"./green_on\"><button>Ligar Verde</button></form>\n"
+             "<form action=\"./green_off\"><button>Desligar Verde</button></form>\n"
+             "</body>\n"
+             "</html>\n");
+
+tcp_write(tpcb, html, strlen(html), TCP_WRITE_FLAG_COPY);
+tcp_output(tpcb);
+free(request);
+    
+pbuf_free(p);
+return ERR_OK;
 }
